df03385a07fb3451b2c05fc706d4de129062ec23
0xNeon-opensource <90662438+i-am-neon@users.noreply.github.com>
2024-10-05 19:12:29 -0500

    actually fix .gba crashing by temporarily commenting out ups patching

--- MAKEHACK.bat
+++ MAKEHACK.bat
@@ -5,19 +5,14 @@ echo ---------------------------
 
 set "currDir=%~dp0"
 
-copy "%currDir%WithAnimations.gba" "%currDir%temp.gba"
+copy "%currDir%WithAnimations.gba" "%currDir%hack.gba"
 
 echo ---------------------------
 echo Assembling ROM. Please wait...
 echo ---------------------------
 
-"%currDir%EA\ColorzCore.exe" A FE8 "-output:%currDir%temp.gba" "-input:%currDir%Ultrafile.event" "--nocash-sym"
-
-@REM for some reason it needs to be a different name to work...
-copy "%currDir%temp.gba" "%currDir%hack.gba"
+"%currDir%EA\ColorzCore.exe" A FE8 "-output:%currDir%hack.gba" "-input:%currDir%Ultrafile.event" "--nocash-sym"
 
 "%currDir%sym\SymCombo.exe" "%currDir%hack.sym" "%currDir%hack.sym" "%currDir%Clean.sym"
-"%currDir%ups\ups.exe" diff -b "%currDir%Clean.gba" -m "%currDir%hack.gba" -o "%currDir%hack.ups"
-
-@REM Delete temp.gba
-del "%currDir%temp.gba"
+@REM TODO: fix. For some reason the act of making the UPS patch causes the ROM to be corrupted.
+@REM "%currDir%ups\ups.exe" diff -b "%currDir%Clean.gba" -m "%currDir%hack.gba" -o "%currDir%hack.ups"
