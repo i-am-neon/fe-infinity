829cbcfc8c731ac14d5dcdb51e760f24fdf224a2
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-10-06 05:13:17 -0500

    run some assembling in parallel

--- AssembleGraphics.sh
+++ AssembleGraphics.sh
@@ -3,41 +3,44 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Trap SIGINT (Ctrl+C) and kill all background processes
+trap 'kill 0' SIGINT
+
 # Get the directory where the script is located
 currDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # AssembleGraphics is at the top level
 tools_dir="$currDir/MacOSTools"
 
+# Assemble icons in parallel
 cd "$currDir/Graphics/ItemIcons"
-
 echo "Assembling icons..."
+./AssembleIcons.sh "$tools_dir" &
 
-./AssembleIcons.sh "$tools_dir"
-
+# Assemble battle palettes in parallel
 cd "$currDir/Graphics/BattlePalettes"
-
 echo "Assembling battle palettes..."
+./AssemblePalettes.sh "$tools_dir" &
 
-./AssemblePalettes.sh "$tools_dir"
-
+# Assemble class cards in parallel
 cd "$currDir/Graphics/ClassCards"
-
 echo "Assembling class cards..."
+./AssembleCards.sh "$tools_dir" &
 
-./AssembleCards.sh "$tools_dir"
-
+# Assemble map sprites in parallel
 cd "$currDir/Graphics/MapSprites"
-
 echo "Assembling map sprites..."
+./AssembleSprites.sh "$tools_dir" &
 
-./AssembleSprites.sh "$tools_dir"
-
+# Assemble portraits in parallel
 cd "$currDir/Graphics/Portraits"
-
 echo "Assembling portraits..."
+./AssemblePortraits.sh "$tools_dir" &
 
-./AssemblePortraits.sh "$tools_dir"
-
+# Assemble world map in parallel
 cd "$currDir/Graphics/WorldMap"
+./AssembleSmallWM.sh "$tools_dir" &
 
-./AssembleSmallWM.sh "$tools_dir"
+# Wait for all background jobs to finish
+wait
+
+echo "All assemblies complete!"
