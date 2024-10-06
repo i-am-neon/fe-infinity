57b4d836ef9f6b346dcc7b8a23b36330a0190eee
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-10-06 04:05:58 -0500

    use mac script for AssembleIcons.sh

--- Graphics/ItemIcons/AssembleIcons.sh
+++ Graphics/ItemIcons/AssembleIcons.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# png2dmp.exe: Calls png2dmp.exe for all .png files in folder & subfolders
+# png2dmp: Calls ./Png2DmpMac for all .png files in folder & subfolders
 # Does not call png2dmp for files where the existing .dmp file is newer than the .png file
 
 FILE_MATCH="*.png"
 script_dir="$(dirname "$0")"
-png2dmp="$script_dir/Png2Dmp.exe"
+png2dmp="$script_dir/Png2DmpMac"
 
 find "$script_dir" -type f -name "$FILE_MATCH" -print0 | while IFS= read -r -d '' F; do
   SHOULD_COMPILE=0
@@ -19,11 +19,8 @@ find "$script_dir" -type f -name "$FILE_MATCH" -print0 | while IFS= read -r -d '
   fi
   if [ "$SHOULD_COMPILE" -eq 1 ]; then
     echo "Assembling \"$(basename "$F")\"..."
-    # Convert paths to Windows format
-    PNG2DMP_WIN=$(winepath -w "$png2dmp")
-    IMAGE_WIN=$(winepath -w "$F")
-    # Run the command without changing directories
-    wine "$PNG2DMP_WIN" "$IMAGE_WIN"
+    # Run the Mac version of png2dmp
+    "$png2dmp" "$F"
   fi
 done
 
