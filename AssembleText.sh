f03001d12ec0bbfbb915d8207dc555cad2d9d78c
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-10-06 01:31:57 -0500

    Use mac-friendly file parser

--- AssembleText.sh
+++ AssembleText.sh
@@ -6,6 +6,6 @@ cd "$script_dir/Text"
 
 echo "Assembling text..."
 
-python3 ./textprocess-classic-narrow.py "TextBuildfile.s" --parser-exe "wine ./ParseFile.exe" --installer InstallTextData.event --definitions TextDefinitions.event
+python3 ./textprocess-classic-narrow.py "TextBuildfile.s" --parser-exe "./ParseFileMac" --installer InstallTextData.event --definitions TextDefinitions.event
 
 echo "Assembling Text Complete"
