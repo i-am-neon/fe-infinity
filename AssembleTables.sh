f48a50fbcd04968088510a6cdcb46d6b8f44d13e
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-10-05 23:30:06 -0500

    use python script in AssembleTables.sh

--- AssembleTables.sh
+++ AssembleTables.sh
@@ -6,7 +6,6 @@ cd "$script_dir/CSV/Tables"
 
 echo "Assembling Tables..."
 
-# Run the TableManager.exe via wine, piping an empty line to it
-echo | wine ./TableManager.exe TableOptions.s TableInstaller.event --error_on_missing
+python TableManager.py TableOptions.s TableInstaller.event --error_on_missing
 
 echo "Assembling Tables Complete"
