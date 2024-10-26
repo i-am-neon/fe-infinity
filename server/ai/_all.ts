5f99cbc61f3d63ace97352f5b967c8775d5de8dc
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-10-26 14:17:43 -0500

    refactor allAI to run ai calls in parallel

--- server/ai/_all.ts
+++ server/ai/_all.ts
@@ -2,14 +2,22 @@ import generateWorldSummary from "@/ai/generate-world-summary.ts";
 import { allPortraitOptions, worldIdeaExample } from "@/testData/ai.ts";
 import generateMainCharacterIdea from "@/ai/generate-main-character-idea.ts";
 import choosePortrait from "@/ai/choose-portrait.ts";
+import assembleCharacterCsvData from "@/ai/assemble-rom-character/assemble-character-csv-data/assemble-character-csv-data.ts";
 
 export default async function allAI(): Promise<void> {
   const worldSummary = await generateWorldSummary(worldIdeaExample);
   const mainCharacterIdea = await generateMainCharacterIdea({ worldSummary });
-  const chosenPortrait = await choosePortrait({
-    portraitOptions: allPortraitOptions,
-    characterIdea: mainCharacterIdea,
-  });
+
+  const [chosenPortrait, characterCsvData] = await Promise.all([
+    choosePortrait({
+      portraitOptions: allPortraitOptions,
+      characterIdea: mainCharacterIdea,
+    }),
+    assembleCharacterCsvData(mainCharacterIdea),
+  ]);
+
+  console.log(JSON.stringify(chosenPortrait, null, 2));
+  console.log(JSON.stringify(characterCsvData, null, 2));
 }
 
 if (import.meta.main) {
