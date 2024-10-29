89e0f695025c42dab15f8043b48ab229463856a1
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-10-28 21:11:12 -0500

    assemble all romCharacters

--- server/ai/_all.ts
+++ server/ai/_all.ts
@@ -6,6 +6,7 @@ import assembleCharacterCsvData from "@/ai/assemble-rom-character/assemble-chara
 import type { RomChapter } from "@/types/RomChapter.ts";
 import generateStoryArc from "@/ai/generate-story-arc.ts";
 import { assignMultiplePortraits } from "@/ai/portraits/assign-multiple-portraits.ts";
+import assembleRomCharacter from "@/ai/assemble-rom-character/assemble-rom-character.ts";
 
 export default async function allAI({
   worldIdea,
@@ -15,19 +16,17 @@ export default async function allAI({
   const worldSummary = await generateWorldSummary(worldIdea);
   const mainCharacterIdea = await generateMainCharacterIdea({ worldSummary });
 
-  const [mainCharacterChosenPortrait, mainCharacterCsvData, storyArc] =
-    await Promise.all([
-      choosePortrait({
-        portraitOptions: allPortraitOptions,
-        characterIdea: mainCharacterIdea,
-      }),
-      assembleCharacterCsvData(mainCharacterIdea),
-      generateStoryArc({
-        worldSummary,
-        mainCharacterIdea,
-        numberOfChapters: 3,
-      }),
-    ]);
+  const [mainCharacterChosenPortrait, storyArc] = await Promise.all([
+    choosePortrait({
+      portraitOptions: allPortraitOptions,
+      characterIdea: mainCharacterIdea,
+    }),
+    generateStoryArc({
+      worldSummary,
+      mainCharacterIdea,
+      numberOfChapters: 3,
+    }),
+  ]);
 
   const allCharacterIdeas = storyArc.chapterIdeas
     .map((chapterIdea) => [
@@ -39,12 +38,31 @@ export default async function allAI({
   const remainingPortraitOptions = allPortraitOptions.filter(
     (p) => p.originalImageName !== mainCharacterChosenPortrait.originalImageName
   );
-  const [characterPortraits] = await Promise.all([
+
+  // TODO: could speed up assigning portraits by separating streams by gender and age and running those in parallel, only feeding in filtered options from the appropriate gender and age
+  const [allCharacterPortraits, mainRomCharacter] = await Promise.all([
     assignMultiplePortraits({
-      characterIdeas: allCharacterIdeas,
       portraitOptions: remainingPortraitOptions,
+      characterIdeas: allCharacterIdeas,
+    }),
+    assembleRomCharacter({
+      characterIdea: mainCharacterIdea,
+      portraitMetadata: mainCharacterChosenPortrait,
     }),
   ]);
+
+  // Map each character idea with corresponding portrait metadata and run in parallel
+  const romCharacterPromises = allCharacterPortraits.map(
+    ({ character, portrait }) =>
+      assembleRomCharacter({
+        characterIdea: character,
+        portraitMetadata: portrait,
+      })
+  );
+
+  // Wait for all promises to resolve and return the results
+  const romCharacters = await Promise.all(romCharacterPromises);
+  const allRomCharacters = [mainRomCharacter, ...romCharacters];
 
   return [];
 }
