7cee106130d8c52f893edb848db76cf2fbde48a8
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-10-29 13:25:49 -0500

    Update assemble-chapter-event.ts

--- server/ai/assemble-chapter-event/assemble-chapter-event.ts
+++ server/ai/assemble-chapter-event/assemble-chapter-event.ts
@@ -1,14 +1,44 @@
 import type { StoryArc } from "@/types/ai/StoryArc.ts";
 import type { ChapterEvent } from "@/types/ChapterEvent.ts";
-import { storyArcExample } from "@/testData/ai.ts";
+import { characterIdeaExample, storyArcExample } from "@/testData/ai.ts";
+import generateScene from "@/ai/assemble-chapter-event/generate-scene.ts";
+import type { CharacterIdea } from "@/types/ai/CharacterIdea.ts";
 
 export default async function assembleChapterEvent({
   storyArc,
   chapterNumberToAssemble,
+  existingPartyCharacters,
 }: {
   storyArc: StoryArc;
   chapterNumberToAssemble: number;
+  existingPartyCharacters: CharacterIdea[];
 }): Promise<ChapterEvent> {
+  const thisChapterIdea = storyArc.chapterIdeas[chapterNumberToAssemble];
+
+  const {
+    sceneContent: preBattleSceneContent,
+    textSceneName: preBattleTextSceneName,
+    textSceneContent: preBattleTextSceneContent,
+  } = await generateScene({
+    sceneOverview: thisChapterIdea.preChapterScene,
+    existingPartyCharacters,
+    newPlayableCharacters: thisChapterIdea.newPlayableCharacters || [],
+    boss: thisChapterIdea.boss,
+    preOrPostBattle: "pre-battle",
+  });
+
+  const {
+    sceneContent: postBattleSceneContent,
+    textSceneName: postBattleTextSceneName,
+    textSceneContent: postBattleTextSceneContent,
+  } = await generateScene({
+    sceneOverview: thisChapterIdea.postChapterScene,
+    existingPartyCharacters,
+    newPlayableCharacters: thisChapterIdea.newPlayableCharacters || [],
+    boss: thisChapterIdea.boss,
+    preOrPostBattle: "post-battle",
+  });
+
   return {
     eventDataReference:
       chapterNumberToAssemble === 0
@@ -20,10 +50,10 @@ export default async function assembleChapterEvent({
     miscBasedEvents: undefined,
     trapData: undefined,
     units: undefined,
-    beginningScene: "",
-    endingScene: "",
+    beginningScene: preBattleSceneContent,
+    endingScene: postBattleSceneContent,
     localDefinitions: [""],
-    text: "",
+    text: `## ${preBattleTextSceneName}\n[ConversationText]\n${preBattleTextSceneContent}[X]\n\n## ${postBattleTextSceneName}\n[ConversationText]\n${postBattleTextSceneContent}[X]`,
   };
 }
 
@@ -31,6 +61,7 @@ if (import.meta.main) {
   const res = await assembleChapterEvent({
     storyArc: storyArcExample,
     chapterNumberToAssemble: 0,
+    existingPartyCharacters: [characterIdeaExample],
   });
   console.log(JSON.stringify(res, null, 2));
 }
