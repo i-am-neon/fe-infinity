af6132919b604fbefa96f36b3d2469f61d306ac8
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-10-28 20:00:18 -0500

    Update _all.ts

--- server/ai/_all.ts
+++ server/ai/_all.ts
@@ -4,6 +4,7 @@ import generateMainCharacterIdea from "@/ai/generate-main-character-idea.ts";
 import choosePortrait from "@/ai/choose-portrait.ts";
 import assembleCharacterCsvData from "@/ai/assemble-rom-character/assemble-character-csv-data/assemble-character-csv-data.ts";
 import type { RomChapter } from "@/types/RomChapter.ts";
+import generateStoryArc from "@/ai/generate-story-arc.ts";
 
 export default async function allAI({
   worldIdea,
@@ -13,18 +14,23 @@ export default async function allAI({
   const worldSummary = await generateWorldSummary(worldIdea);
   const mainCharacterIdea = await generateMainCharacterIdea({ worldSummary });
 
-  const [mainCharacterChosenPortrait, mainCharacterCsvData] = await Promise.all(
-    [
+  const [mainCharacterChosenPortrait, mainCharacterCsvData, storyArc] =
+    await Promise.all([
       choosePortrait({
         portraitOptions: allPortraitOptions,
         characterIdea: mainCharacterIdea,
       }),
       assembleCharacterCsvData(mainCharacterIdea),
-    ]
-  );
+      generateStoryArc({
+        worldSummary,
+        mainCharacterIdea,
+        numberOfChapters: 3,
+      }),
+    ]);
 
   console.log(JSON.stringify(mainCharacterChosenPortrait, null, 2));
   console.log(JSON.stringify(mainCharacterCsvData, null, 2));
+  console.log(JSON.stringify(storyArc, null, 2));
 
   return [];
 }
