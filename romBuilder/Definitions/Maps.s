4bf589575548c15ba6ebee2bcda8189f8aede5dd
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-11-15 12:56:05 -0600

    update map options

--- romBuilder/Definitions/Maps.s
+++ romBuilder/Definitions/Maps.s
@@ -1,5 +1,5 @@
 Dummy 0x04
-NoblesEvilDoers11800191A_Map
-NoblesEvilDoers11800191A_Changes
 NoblesEvilDoers201004C03_Map
 NoblesEvilDoers201004C03_Changes
+NoblesEvilDoers11800191A_Map
+NoblesEvilDoers11800191A_Changes
